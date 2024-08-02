a5657f77c6aeee91f8e695fffff3840edfcedb7a
Nikola <128964319+NikolaM4@users.noreply.github.com>
2024-08-02 20:51:08 +0200

    Update is-anagram.cpp

--- Arrays and Hashings/is-anagram.cpp
+++ Arrays and Hashings/is-anagram.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+        
+        unordered_map<char,int> s_occurencies;
+        unordered_map<char,int> t_occurencies;
+
+        for(int i = 0; i < s.size(); i++)
+        {
+            s_occurencies[s[i]]++;
+            t_occurencies[t[i]]++;
+        }
+
+        for(int i = 0; i < s.size(); i++)
+            if(s_occurencies[s[i]] != t_occurencies[s[i]]) return false;
+        return true;
+    }
+};
 
